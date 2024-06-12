9f365a288866e5f3bfa872efcf1b00bca6f6ab51
rajatjoe <119100839+rajatjoe@users.noreply.github.com>
2024-06-12 21:10:19 +0530

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,46 @@
+const reviews = [
+  {
+    id: 1,
+    name: "Rajat Goswami",
+    job: "App & Web Developer",
+    image:process.env.PUBLIC_URL + '/assets/Rajat.jpg',
+    text: "I have had the pleasure of working with this team on several projects, and I am consistently impressed with their technical expertise and ability to deliver quality solutions on time and within budget. They are a true partner and an asset to any project.",
+  },
+  {
+    id: 2,
+    name: "Rushi Shah",
+    job: "App & Web Developer",
+    image:process.env.PUBLIC_URL + '/assets/Rushi.jpg',
+    text: "I have been working with this company for several years now, and I have always been impressed with their creativity and attention to detail. They are true professionals who take pride in their work and always go above and beyond to deliver exceptional results.",
+  },
+  {
+    id: 3,
+    name: "Samraat Pratap Singh",
+    job: "Marketing Manager",
+    image:process.env.PUBLIC_URL + '/assets/Samraat.jpg',
+    text: "I am thrilled with the results of our recent marketing campaign, and it wouldn't have been possible without the hard work and dedication of the entire team. Thank you for your exceptional work!",
+  },
+  {
+    id: 4,
+    name: "Mohit Ginglani",
+    job: "Data Analyst",
+    image:process.env.PUBLIC_URL + '/assets/Mohit.jpg',
+    text: "They are true professionals who take pride in their work and always go above and beyond to deliver exceptional results. Collaborating with this team has been a rewarding experience, as they leverage data-driven insights to enhance their innovative solutions and achieve remarkable outcomes.",
+  },
+  {
+    id: 5,
+    name: "Nikhil Raval",
+    job: "UI & UX Designer",
+    image:process.env.PUBLIC_URL + '/assets/Nikhil.jpg',
+    text: "Over the years, this company's creativity and attention to detail in UI/UX design have always impressed me. Their designs are both visually stunning and user-friendly, enhancing our digital presence. They are true professionals who consistently deliver exceptional results..",
+  },
+  {
+    id: 6,
+    name: "Vaibhav Garg",
+    job: "Funding Manager",
+    image:process.env.PUBLIC_URL + '/assets/Vaibhav.jpg',
+    text: "I've been impressed with this company's dedication to fundraising over the years. They excel at connecting with donors and building lasting relationships. Their strategic and creative approach ensures we achieve remarkable results, always going above and beyond to support our mission.",
+  },
+];
+
+export default reviews;
